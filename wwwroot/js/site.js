16ea03e1f9b4046005112efe14f0ebd497f46847
Thanh tra <tranthe150186@fpt.edu.vn>
2021-09-14 23:37:13 +0700

    comit login

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+  const signUpButton = document.getElementById('signUp');
+ const signInButton = document.getElementById('signIn');
+const container = document.getElementById('login_container');
+
+ signUpButton.addEventListener('click', () => {
+ 	container.classList.add("right-panel-active");
+ });
+
+ signInButton.addEventListener('click', () => {
+ 	container.classList.remove("right-panel-active");
+ });
+const searchBtn = document.querySelector(".fa-search");
+const seacrhBox = document.querySelector(".search");
+
+searchBtn.addEventListener("click", myfunction);
+function myfunction() {
+    seacrhBox.classList.add("active");
+}
